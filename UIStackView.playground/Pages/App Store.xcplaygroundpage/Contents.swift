2505971d98a72af4daf1be77a57bd7589e818c2b
dasdom <dom@dasdom.de>
2015-06-28 16:10:11 +0200

    Added size classes example

--- UIStackView.playground/Pages/App Store.xcplaygroundpage/Contents.swift
+++ UIStackView.playground/Pages/App Store.xcplaygroundpage/Contents.swift
@@ -28,6 +28,13 @@ devNameLabel.font = UIFont.systemFontOfSize(13)
 devNameLabel.text = "Dominik Hauser"
 devNameLabel.backgroundColor = UIColor.redColor()
 
+let buyButton = UIButton(type: .System)
+buyButton.setTitle("0,99 €", forState: .Normal)
+buyButton.titleLabel?.font = UIFont.boldSystemFontOfSize(13)
+buyButton.layer.cornerRadius = 5
+buyButton.layer.borderColor = buyButton.tintColor.CGColor
+buyButton.layer.borderWidth = 1
+
 let headerView = UIView(frame: CGRect.zeroRect)
 //headerView.backgroundColor = UIColor.brownColor()
 
@@ -35,9 +42,13 @@ if #available(iOS 9, *) {
   let metaDataStackView = UIStackView(arrangedSubviews: [appNameLabel, devNameLabel])
   metaDataStackView.axis = .Vertical
   
-  let headerStackView = UIStackView(arrangedSubviews: [iconImageView, metaDataStackView])
+  let leftHeaderStackView = UIStackView(arrangedSubviews: [metaDataStackView, buyButton])
+  leftHeaderStackView.axis = .Vertical
+  leftHeaderStackView.distribution = UIStackViewDistribution.EqualCentering
+  
+  let headerStackView = UIStackView(arrangedSubviews: [iconImageView, leftHeaderStackView])
   headerStackView.translatesAutoresizingMaskIntoConstraints = false
-  headerStackView.alignment = .Top
+//  headerStackView.alignment = .Top
   headerStackView.layoutMarginsRelativeArrangement = true
   headerStackView.spacing = 10
   headerView.addSubview(headerStackView)
@@ -51,7 +62,7 @@ if #available(iOS 9, *) {
   constraints += NSLayoutConstraint.constraintsWithVisualFormat("|[mainStackView]|", options: [], metrics: nil, views: views)
   constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|[mainStackView]|", options: [], metrics: nil, views: views)
   constraints += NSLayoutConstraint.constraintsWithVisualFormat("|-10-[headerStackView]-10-|", options: [], metrics: nil, views: views)
-  constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-10-[headerStackView]-10-|", options: [], metrics: nil, views: views)
+  constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-10-[headerStackView]", options: [], metrics: nil, views: views)
   constraints.append(iconImageView.widthAnchor.constraintEqualToConstant(100))
   constraints.append(iconImageView.heightAnchor.constraintEqualToConstant(100))
   NSLayoutConstraint.activateConstraints(constraints)
